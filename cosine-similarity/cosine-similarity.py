95f3ef31b6b8411dd750d597f7234dc56a2456f1
Saksham Sharma <106482027+1sakshm@users.noreply.github.com>
2026-09-12 18:58:03 +0530

    Solve cosine-similarity

---
+++ cosine-similarity/cosine-similarity.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def cosine_similarity(a: list, b: list) -> float:
+    """
+    Returns the cosine similarity as a Python float.
+    """
+    dot=np.dot(a,b)
+    a1=np.linalg.norm(a)
+    b1=np.linalg.norm(b)
+    return float(np.nan_to_num(dot/(a1*b1)))
+    # Write code here
+    return np.cos(a,b)
+    pass
